1d69874569ed775b15c669b18f766fe593d09af7
Azure 238 <Azure238@itstep.edu.az>
2025-05-15 11:08:53 +0400

    Test env integrated

---
+++ WebApi.Test/ProductTests.cs
@@ -0,0 +1,92 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTest;
+
+namespace WebApi.Test
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        private WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+
+                builder.ConfigureServices(services =>
+                {
+
+                });
+
+            });
+
+            _client = _factory.CreateClient();
+        }
+
+        [Test]
+        public async Task GetProducts_ReturnsOkResponse()
+        {
+            //Arrenge 
+            var response = await _client.GetAsync("/api/products?top=0");
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+
+            var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+
+            Assert.That(products != null);
+        }
+
+        [Test]
+        public async Task GetProductsWithTop_ReturnsCorrectProduct()
+        {
+            var count = 5;
+            //Arrenge 
+            var response = await _client.GetAsync($"/api/products?top={count}");
+
+            var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+
+            //Assert
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.That(products != null);
+            Assert.That(products?.Count <= count);
+        }
+
+        [Test]
+        public async Task PostProduct_AddsNewProduct()
+        {
+            //Arrenge 
+            var newProduct = new Product
+            {
+                Name = "New Product",
+                Price = 500
+            };
+
+            //Act
+
+            var response = await _client.PostAsJsonAsync("/api/products", newProduct);
+
+            //Assert
+
+            response.EnsureSuccessStatusCode();
+            var createdProduct = await response.Content.ReadFromJsonAsync<Product>();
+
+            Assert.That(createdProduct != null);
+            Assert.That(newProduct.Name+"a", Is.EqualTo(createdProduct?.Name));
+
+        }
+
+    }
+}
